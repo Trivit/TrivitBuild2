3e9ee7c931f37b2a9b824904f6a824a5cb3eea42
Brendan Kelly <bpkelly@umass.edu>
2017-03-22 10:46:39 -0400

    first commit

---
+++ Trivit2Watch Extension/MovetimeController.swift
@@ -0,0 +1,44 @@
+//
+//  MovetimeController.swift
+//  TrivitBuild2
+//
+//  Created by TRIVIT on 3/22/17.
+//  Copyright © 2017 TRIVIT. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class MovetimeController: WKInterfaceController {
+    
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+    
+}
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
---
+++ TrivitBuild2/GeneralViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TabbedProject
+//
+//  Created by TRIVIT on 3/22/17.
+//  Copyright © 2017 TRIVIT. All rights reserved.
+//
+
+import UIKit
+
+class GeneralViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
